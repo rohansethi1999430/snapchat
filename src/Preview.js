868d7d27a17282ea847cb26455d024661c464f21
Rohan <rohansethi1999430@icloud.com>
2021-01-21 22:02:43 +0530

    useEffect function incase there is no image and also closeIcon implemented

--- src/Preview.js
+++ src/Preview.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import { resetCameraImage, selectCameraImage } from './features/cameraSlice'
 import "./Preview.css"
+import CloseIcon from '@material-ui/icons/Close'
 function Preview() {
+    const cameraImage = useSelector(selectCameraImage)
+    const history=useHistory()
+    const dispatch = useDispatch()
+    useEffect(() => {
+        if(!cameraImage){
+            history.replace('/')
+        }
+        
+    }, [cameraImage,history])
+
+    const closePreview =()=>{
+    dispatch(resetCameraImage())
+    history.replace('/')
+}
+
     return (
         <div className='preview'>
+            <CloseIcon  onClick={closePreview} className='preview__close'/>
             
+            <img src={cameraImage} alt=""/>
         </div>
     )
 }
