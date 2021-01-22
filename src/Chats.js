4f2ec1ef076ca2091fbe0ecadd64b9a4a2a18152
Rohan <rohansethi1999430@icloud.com>
2021-01-22 11:55:41 +0530

    Chats.js header styling

--- src/Chats.js
+++ src/Chats.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import './Chats.css'
+import {Avatar} from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search';
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
 
 function Chats() {
     return (
-        <div>
+        <div className="chats">
+            <div className="chats__header">
+                <Avatar className="chats__avatar"/>
+                <div className="chats__search">
+                <SearchIcon />
+                <input type="text" placeholder="Friends"/>
+                </div>
+                <ChatBubbleIcon className='chats__chatIcon'/>
+            </div>
             
         </div>
     )
