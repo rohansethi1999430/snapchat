76733ce0a2767931bb43b12ba252ab1c840e1c1d
Rohan <rohansethi1999430@icloud.com>
2021-02-01 20:43:27 +0530

    User LogIn Page created and Login.css also

--- src/Login.js
+++ src/Login.js
@@ -11,8 +11,10 @@ function Login() {
     
     return (
         <div className='login'>
-            <img src="https://scx2.b-cdn.net/gfx/news/2017/1-snapchat.jpg" alt=""/>
-            <Button variant='outline' onClick={signIn}>Sign In</Button>
+           <div className="login__container">
+           <img src="https://scx2.b-cdn.net/gfx/news/2017/1-snapchat.jpg" alt=""/>
+            <Button variant='outlined' onClick={signIn}>Sign In</Button>
+           </div>
         </div>
     )
 }
